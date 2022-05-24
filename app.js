d327916fd221f7b4de6ebf8b81ed33f3f427844e
alexjoshuaboj <alejandrobojneag@gmail.com>
2022-05-24 20:07:36 +0200

    delete cors package

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const cors = require('cors');
 
 const moviesRouter = require("./routes/peliculas");
 const usersRouter = require("./routes/users");
@@ -15,22 +14,15 @@ const docu = require("./documentation/documentation.json");
 
 const app = express();
 
-let corsOptions = {
-  origin: function (origin, callback) {
-    db.loadOrigins(function (error, origins) {
-      callback(error, origins)
-    })
-  }
-}
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/peliculas", cors(corsOptions), moviesRouter);
-app.use("/usuarios", cors(corsOptions),usersRouter);
-app.use("/listas", cors(corsOptions), listaRouter);
+app.use("/peliculas", moviesRouter);
+app.use("/usuarios",usersRouter);
+app.use("/listas", listaRouter);
 
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
